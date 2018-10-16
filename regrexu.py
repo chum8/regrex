b28b0c88cb83217ece2ba678b89e359147d4d3ed
forgottenfive <forgottenfive@outlook.com>
2018-10-15 21:41:42 -0500

    Added fast track mode to regrexu.py

--- regrexu.py
+++ regrexu.py
@@ -1,5 +1,15 @@
 # [----IMPORT LIBRARIES----]
-import requests
+import requests, sys
+from subprocess import call
+
+# [----CHECK FOR OPENING ARGS----]
+if len(sys.argv) == 2:
+    # put the system on fast track if we passed in a URl in the CLI
+    ftrack = True
+    my_url = sys.argv[1]
+else:
+    ftrack = False
+    my_url = 'http://www.example.com'
 
 # [----FUNCTIONS----]
 # get contents of a url
@@ -18,42 +28,57 @@ def file_write(my_txt, my_file, my_exit_message):
         exit()
 
 # generate a filename for the write to file
-def filename_maker(my_url):
+def filename_maker(my_url, my_dir):
     my_url = my_url.replace('://','_')
     my_url = my_url.replace('//','_')
     my_url = my_url.replace('/','_')
     my_url = my_url.replace('.','_')
-    return 'urllog/log_' + my_url
+    return my_dir + '/log_' + my_url
 
 # [----PROGRAM BODY----]
 #defaults
-my_url = 'http://www.example.com'
 my_exit_message = '\nExiting RegRex URL Helper.'
+my_dir = 'urllog'
 
-# load opening image
-print(open('regrexu_img.txt', 'r').read())
+if ftrack:
+    print('RegRex URL Helper is running in fast track mode...\nplease wait while',my_url,'is scraped.')
 
-# get url contents
-my_url = input('URL contents will be scraped. Enter a URL to try: ')
-my_txt = retrieve(my_url)
-print("Successfully loaded", my_url)
+    try:
+        # get url contents
+        my_txt = retrieve(my_url)
+        print("URL retrieved...")
 
-# write url contents to a file
-my_file = filename_maker(my_url)
-file_write(my_txt, my_file, my_exit_message)
-print("Successfully wrote contents of", my_url, "to", my_file)
+        # write url contents to a file
+        my_file = filename_maker(my_url, my_dir)
+        file_write(my_txt, my_file, my_exit_message)
+        print("File written...")
 
-# see if user wants to view contents
-if input("Would you like to echo the text of " + my_url + " to your terminal window before passing the contents to RegRex? (Yn) ").lower() == 'y':
-    print("\nBody of", my_url)
-    print("[BEGIN RECORD]")
-    print(my_txt)
-    print("[RECORD END]")
+    except:
+        print("There was a problem.  Try using RegRex URL Helper in normal mode or check your URL!")
+        exit()
+
+else:
+    # load opening image
+    print(open('regrexu_img.txt', 'r').read())
+
+    # get url contents
+    my_url = input('URL contents will be scraped. Enter a URL to try: ')
+    my_txt = retrieve(my_url)
+    print("Successfully loaded", my_url)
+
+    # write url contents to a file
+    my_file = filename_maker(my_url, my_dir)
+    file_write(my_txt, my_file, my_exit_message)
+    print("Successfully wrote contents of", my_url, "to", my_file)
+
+    # see if user wants to view contents
+    if input("Would you like to echo the text of " + my_url + " to your terminal window before passing the contents to RegRex? (Yn) ").lower() == 'y':
+        print("\nBody of", my_url)
+        print("[BEGIN RECORD]")
+        print(my_txt)
+        print("[RECORD END]")
 
 # pass contents into regrex.py for further processing
-
-# exit
-print(my_exit_message)
-exit()
+call('py regrex.py ' + my_file)
 # [----END OF PROGRAM----]
 
